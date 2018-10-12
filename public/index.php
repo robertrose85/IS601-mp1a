d6ac6c92ff4f183d41873928b94e5d1503c5b3d3
Bob Rose <rr637@njit.edu>
2018-10-11 20:03:31 -0400

    Fix: Created project branch added classes for main, csv, table, system and worked out flow of program

--- public/index.php
+++ public/index.php
@@ -6,14 +6,49 @@
  * Time: 6:31 PM
  */
 
+main::start();
 
-echo 'Test Branch';
-/**echo 'testing Github -> Heroku Connection';*/
+class main {
 
-/**Testing Commits with Branching*/
+    static public function start(){
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
 
-echo 'Blah Blah';
+/** Read the file */
+class csv {
 
+    static public function getRecords() {
 
+        $records = 'test111';
 
+        return $records;
+
+    }
+
+}
+ /** Make the table */
+class html {
+
+    static public function generateTable($records) {
+
+        $table = $records;
+
+        return $table;
+
+    }
+
+}
+/** Print the table with file data */
+class system {
+
+    static public function printPage($page) {
+
+        echo $page;
+
+    }
+
+}
 
--- readme.md
+++ readme.md
@@ -50,6 +50,20 @@ In terminal -> git commit -m "add composer.json for PHP app detection"
 
 Text file -> google heroku php procfile and look for specifying which webserver to use and setting doc root.
 
+web: vendor/bin/heroku-php-apache2 public/
+
+Webserver Procfile text.
+
 End: 57:30
 
+## Branches
+
+Go to VCS - Git - Create branches. In the lower right you can switch between branch and master.
+
+COMMIT THE BRANCH and you can switch between the master app and the branch app.
+
+Branching allows teams to work on their own versio of the master file and check work
+separately. The merging appends the changes to the master file.
+
+Merging branches into master will automatically commit.
 
