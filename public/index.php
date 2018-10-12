17efbbf6a171388c4b135aac67f909c8aefe1183
Bob Rose <rr637@njit.edu>
2018-10-12 01:22:00 -0400

    Fix: Clean up of code and commented material. Issue with output.

--- public/index.php
+++ public/index.php
@@ -18,23 +18,34 @@ class main {
         $records = csv::getRecords($filename);
         $table = html::generateTable($records);
 
-
         }
 
-        /**
-        $records = csv::getRecords();
-        $table = html::generateTable($records);
-        system::printPage($table);
-         */
 }
 
 class html
 {
     public static function generateTable($records)
     {
+        $count = 0;
+
         foreach ($records as $record) {
-            $array = $record->returnArray();
-            print_r($array);
+            if($count == 0){
+
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+
+            } else{
+                $array = $record->returnArray();
+                $values = array_values($array);
+
+                print_r($values);
+
+            }
+
+            $count++;
         }
     }
 }
@@ -62,25 +73,14 @@ class csv {
                 $fieldNames = $record;
             } else {
                 /* creates an array for each record in the csv using the recordFactory object */
-                $row[] = recordFactory::create($fieldNames, $record);
+                $records[] = recordFactory::create($fieldNames, $record);
             }
             $count++;
         }
 
         fclose($fileRead);
-        return $row;
-
-        /**
-        $make = 'Mazda';
-        $model = 'CX-5';
-        $car = AutomobileFactory::create($make, $model);
-
-        $records[] = $car;
-
-        print_r($records);
-
         return $records;
-         */
+
 
     }
 
@@ -92,15 +92,13 @@ class record
     {
         /* $record is the array coming in from the recordFactory object */
 
-        //print_r($fieldNames);
-        //print_r($values);
         $record = array_combine($fieldNames, $values);
 
         foreach($record as $property => $value){
             $this->createProperty($property, $value);
         }
 
-        print_r($this);
+        //print_r($this);
 
     }
 
@@ -119,69 +117,11 @@ class record
 class recordFactory
 {
     /* statements says that $array has to be an array and if no data is passed, $array is null */
-    public static function create(Array $fieldNames = null, Array $record = null)
+    public static function create(Array $fieldNames = null, Array $values = null)
     {
 
-        $record = new record($fieldNames, $record);
+        $record = new record($fieldNames, $values);
 
         return $record;
     }
 }
-
-
-
-
-
- /** Make the table */
-/**
-class html {
-
-    static public function generateTable($records) {
-
-        $table = $records;
-
-        return $table;
-
-    }
-
-}
-/** Print the table with file data */
-/**
-class system {
-
-    static public function printPage($page) {
-
-        echo $page;
-
-    }
-
-}
-
-class Automobile {
-
-    private $vehicleMake;
-    private $vehicleModel;
-
-    public function __construct($make, $model)
-    {
-
-        $this->vehicleMake = $make;
-        $this->vehicleModel = $model;
-
-    }
-
-    public function getMakeAndModel() {
-
-        return $this->vehicleMake . ' ' . $this->vehicleModel;
-    }
-}
-
-class AutomobileFactory {
-
-    public static function create($make, $model){
-
-        return new Automobile($make, $model);
-    }
- */
-
-
